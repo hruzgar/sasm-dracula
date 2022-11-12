2331a4e781f87507adce57e0b47390b63578cd34
Hasan Rüzgar <hasan.ruezgar@tum.de>
2022-11-12 16:56:31 +0100

    f

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
-unzip JetBrainsMono-2.242.zip
-sudo mv fonts/ttf/JetBrainsMono-*.ttf /usr/share/fonts/
-sudo mv SASM.conf ~/.config/SASM
+sudo mv "fonts/JetBrainsMono*.ttf" "/usr/share/fonts/"
+sudo mv "SASM.conf" "~/.config/SASM"
 echo -e "Please restart your pc for the changes to affect.\nImportant: If you don't restart your PC after running the script, rerunning the script could be neccessery!"
