12a18a59409a01cc1774834aec881d42c77ecb22
Hasan Rüzgar <84530266+hruzgar@users.noreply.github.com>
2022-11-12 22:02:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 sudo mv -f fonts/JetBrainsMono*.ttf /usr/share/fonts/
 sudo mv -f SASM.conf ~/.config/SASM
-echo -e "Please restart your pc for the changes to affect.\nImportant: If you don't restart your PC after running the script, rerunning the script could be neccessery!"
+echo -e "Please restart your pc for the changes to take affect.\nImportant: If you don't restart your PC after running the script, rerunning the script could be neccessary!"
