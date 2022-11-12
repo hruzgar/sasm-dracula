326c0e1e98190eaca0081c92a72c1401fc1ba8ad
Hasan Rüzgar <84530266+hruzgar@users.noreply.github.com>
2022-11-12 21:11:20 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo mv fonts/JetBrainsMono*.ttf /usr/share/fonts/
-sudo mv SASM.conf ~/.config/SASM
-echo -e "Please restart your pc for the changes to affect.\nImportant: If you don't restart your PC after running the script, rerunning the script could be neccessery!"
